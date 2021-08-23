4a00a5d692d9fb60356ae5a5918c49c69bb3a669
Rob Chojnacki <robchojnacki43@gmail.com>
2021-08-22 22:36:12 -0400

    Search now using api, no default users shown

--- .history/src/App_20210822215308.js
+++ .history/src/App_20210822215308.js
@@ -13,28 +13,15 @@ class App extends Component {
     loading: false,
   }
 
-  // async componentDidMount() {
-  //   this.setState({ loading: true });
-
-  //   const res = await axios.get(`https://api.github.com/users?client_id=$
-  //     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-  //     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-   
-  //   this.setState({
-  //     users: res.data,
-  //     loading: false
-  //   });
-  // }
-
-  searchUsers = async text => {
+  async componentDidMount() {
     this.setState({ loading: true });
 
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
+    const res = await axios.get(`https://api.github.com/users?client_id=$
       {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
       {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-  
+   
     this.setState({
-      users: res.data.items,
+      users: res.data,
       loading: false
     });
   }
@@ -44,7 +31,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
           <div className="container">
-            <Search searchUsers={this.searchUsers} />
+            <Search />
             <Users loading={this.state.loading} users={this.state.users} />
           </div>
       </div>
